f60b972b4ccfa1293adc3f6e743526f02f1c66a3
dan <3394219182@qq.com>
2025-11-17 15:14:03 +0800

    删除python package

--- python_package/CMakeFiles/CM_pkg.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CM_pkg.
-# This may be replaced when dependencies are built.
--- python_package/test.py
+++
@@ -1,6 +0,0 @@
-import os
-os.add_dll_directory(r"C:\Program Files\mingw64\bin")
-
-import CM_pkg
-
-print(CM_pkg.add(1, 2))
